bd71206a58802c39decebc588d7584d3270907b9
Ronald Glover <ron881212@gmail.com>
2019-04-14 13:46:11 -0400

    new home route

---
+++ client/src/components/tempNav/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './style.css'
+
+const tempNav = () => {
+    return(
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a className="navbar-brand" href="/">WINC</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/">Parent</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/">Educator</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    )
+}
+
+export default tempNav
