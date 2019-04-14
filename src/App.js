325ae47b9ff4ee944a58a4c184086478646487d6
Ronald Glover <ron881212@gmail.com>
2019-04-14 07:51:46 -0400

    added landing page that links to books

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React, { Component } from 'react';
 import Book from './components/Book'
+import Landing from './components/Landing'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Book />
+      <Router>
+      <div>
+        
+        <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/prettybooks" component={Book} />
+        </Switch>
+      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/tempJombo/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TempJumbo = () => {
+    return(
+        <div className="jumbotron">
+            <div className="container">
+                <h1 className="display-4">Fluid jumbotron</h1>
+                <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            </div>
+        </div>
+        
+    )
+}
+
+export default TempJumbo
