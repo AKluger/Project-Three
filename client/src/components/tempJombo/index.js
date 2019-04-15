4ea7a40947357513d2bf818e9ec3e5c612e6116a
Monetthigpen <monet.thigpen@gmail.com>
2019-04-15 15:49:11 -0400

    updated Landing page style

--- client/src/components/tempJombo/index.js
+++ client/src/components/tempJombo/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import "./style.css"
 
 const TempJumbo = () => {
     return(
-        <div className="jumbotron">
-            <div className="container">
-                <h1 className="display-4">Fluid jumbotron</h1>
-                <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+        <div className="jumbotron jumbo text-center">
+            <div className="container ">
+                <h1 className="display-4">CHOOSE A STORY</h1>
             </div>
         </div>
         
