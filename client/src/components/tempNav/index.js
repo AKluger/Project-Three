5497d5c64b62dae20f0f55406d2784d1bdef81f9
Monetthigpen <monet.thigpen@gmail.com>
2019-04-15 16:17:51 -0400

    made fixed nav

--- client/src/components/tempNav/index.js
+++ client/src/components/tempNav/index.js
@@ -3,7 +3,7 @@ import './style.css'
 
 const tempNav = () => {
   return(
-      <nav className="navbar navbar-expand-lg ">
+      <nav className="navbar fixed-top navbar-expand-lg ">
         <a className="navbar-brand jump" href="/"> <span className="logotxt">WIN<div className="jump">C</div></span></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
