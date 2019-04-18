a07f6a5de22225f0599cf1c35e032c5cb365a7db
kmop <42947972+AKluger@users.noreply.github.com>
2019-04-18 16:18:16 -0400

    created folder for books and matched cover for each route with matching json and style files

--- client/src/Books/prettyCity/prettyCity.js
+++ client/src/Books/prettyCity/prettyCity.js
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
 import { useSpring, animated } from 'react-spring'
-import Nav from '../../Components/Nav';
-import PagesContainer from "../../Components/PagesContainer";
-import Pages from './game.json';
+import Nav from '../../components/Nav';
+import PagesContainer from "../../components/PagesContainer";
+import Pages from './prettyCity.json';
 import FlipPage from "react-flip-page";
 import './style.css'
-import Start from '../../Components/Modal';
-import bin3 from '../tempLibrary/recycle.png'
-import glassBottle from '../../tempImages/glassBottle.png'
+import Start from '../../components/Modal/';
 
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
 const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`
 
-function Book(){
+function PrettyCity(){
   const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
   const [pages] = useState(Pages)
   let [kid, setKid] = useState("")
@@ -47,8 +45,6 @@ function Book(){
               <article  key={page.id}>
                 <div className='page-image' key={key++} onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
                   <animated.img src={eval(kid)} alt='kids' className='kids' style={{ transform: props.xy.interpolate(trans1) }}/>
-                  {page.trash ? (<img src={bin3} alt='bin' className='bin' key={key++}/>) :(<div></div>)}
-                  {page.trash ? (<img src={page.trash} alt='trash' className={page.cssClass} key={key++}/>) :(<div></div>)}
                   <img src={page.imageLink} alt={page.imageTitle} style={theEndStyle} className='main' key={key++}/>
                 </div>
                   <div className="text-center page-text">{page.text.map(line=>(<h2 key={key++}>{line}</h2>))}</div>
@@ -62,6 +58,6 @@ function Book(){
       
 }
 
-export default Book
+export default PrettyCity
 
 // <a href="https://www.freepik.com/free-photos-vectors/food">Food vector created by macrovector - www.freepik.com</a>
