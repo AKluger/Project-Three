4409db05f011b3a72a10da59c48d79d901d9404e
kmop <42947972+AKluger@users.noreply.github.com>
2019-04-21 14:18:04 -0400

    Fix PrettyCity text cutoff and reduce image stretch, create media queries for ipad and imac home screen

--- client/src/Books/prettyCity/prettyCity.js
+++ client/src/Books/prettyCity/prettyCity.js
@@ -5,7 +5,6 @@ import PagesContainer from "../../components/PagesContainer";
 import Pages from './prettyCity.json';
 import FlipPage from "react-flip-page";
 import './style.css'
-import Start from '../../components/Modal/';
 
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
 const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`
@@ -14,41 +13,34 @@ function PrettyCity() {
   const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
   const [pages] = useState(Pages)
   const [score] = useState(0);
-  let [kid, setKid] = useState("")
 
-  const pickCharacter = e => {
-    console.log(e)
-    setKid(kid = e)
-  }
 
-  // const theEndStyle = {
+  const bookStyle = {
+    position: 'relative'
   //   alignItems: 'flex-end',
   //   backgroundColor: '#000',
   //   color: '#fff',
   //   display: 'flex',
   //   justifyContent: 'center',
-  //   padding: 5,
   //   width: '100%',
   //   height: '100%'
-  // };
+  };
 
   let width = window.innerWidth * .8
-  let height = window.innerHeight * 1.11
+  let height = window.innerHeight * 1
   let key = 0
 
   return (
     <div className="pages">
       <Nav />
-      <Start character={pickCharacter} />
       <PagesContainer >
-        <FlipPage orientation='horizontal' className="margin" height={JSON.stringify(height)} width={JSON.stringify(width)}>
+        <FlipPage orientation='horizontal' height={JSON.stringify(height)-102} width={JSON.stringify(width)} style={bookStyle}>
           {pages.map(page => (
             <article key={page.id}>
-              <div className='page-image container-fluid' key={key++} onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
-                <animated.img src={eval(kid)} alt='kids' className='kids' style={{ transform: props.xy.interpolate(trans1) }} />
+              <div className='page-image' key={key++} onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
                 <img src={page.imageLink} alt={page.imageTitle} className='main imgs' key={key++} />
               </div>
-              <div className="text-center page-text">{page.text.map(line => (<h2 key={key++}>{line}</h2>))}</div>
+              <div className="text-center pretty-text pt-2">{page.text.map(line => (<h2 className="pretty-text" key={key++}>{line}</h2>))}</div>
             </article>
           ))}
         </FlipPage>
@@ -60,5 +52,3 @@ function PrettyCity() {
 }
 
 export default PrettyCity
-
-// <a href="https://www.freepik.com/free-photos-vectors/food">Food vector created by macrovector - www.freepik.com</a>
