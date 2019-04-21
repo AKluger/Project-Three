4f7fbdb240e209f8b2a00e17128820f52e6c9196
Alex <aikluger@gmail.com>
2019-04-21 17:00:50 -0400

    media query for text to display on computer and ipad in landscape with no scroll

--- client/src/Books/prettyCity/prettyCity.js
+++ client/src/Books/prettyCity/prettyCity.js
@@ -16,28 +16,26 @@ function PrettyCity() {
 
 
   const bookStyle = {
-    position: 'relative'
-  //   alignItems: 'flex-end',
-  //   backgroundColor: '#000',
-  //   color: '#fff',
-  //   display: 'flex',
-  //   justifyContent: 'center',
-  //   width: '100%',
-  //   height: '100%'
+    position: 'relative',
+      alignItems: 'flex-end',
+      display: 'flex',
+    //   justifyContent: 'center',
+      width: '100%',
+      height: '100%'
   };
 
   let width = window.innerWidth * .8
   let height = window.innerHeight * 1
-  let key = 0
+  let key = 0;
 
   return (
     <div className="pages">
       <Nav />
       <PagesContainer >
-        <FlipPage orientation='horizontal' height={JSON.stringify(height)-102} width={JSON.stringify(width)} style={bookStyle}>
+        <FlipPage orientation='horizontal' height={JSON.stringify(height) - 102} width={JSON.stringify(width)} style={bookStyle}>
           {pages.map(page => (
             <article key={page.id}>
-              <div className='page-image' key={key++} onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
+              <div className='page-image' onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
                 <img src={page.imageLink} alt={page.imageTitle} className='main imgs' key={key++} />
               </div>
               <div className="text-center pretty-text pt-2">{page.text.map(line => (<h2 className="pretty-text" key={key++}>{line}</h2>))}</div>
