5428020d5274301bfb06ab2c5742ae8262f1109c
kmop <42947972+AKluger@users.noreply.github.com>
2019-04-24 00:59:24 -0400

    developing fluid responsiveness for pcity including adjustments to nav

--- client/src/Books/prettyCity/prettyCity.js
+++ client/src/Books/prettyCity/prettyCity.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSpring, animated } from 'react-spring'
 import Nav from '../../components/Nav';
 import PagesContainer from "../../components/PagesContainer";
+import {Container, Image} from "react-bootstrap";
 import Pages from './prettyCity.json';
 import FlipPage from "react-flip-page";
 import './prettyCity.css'
@@ -13,19 +14,21 @@ function PrettyCity() {
   const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
   const [pages] = useState(Pages);
 
-
+  const pageColor = 'linear-gradient(344deg, rgba(255,255,255,1) 0%, rgba(241,241,255,1) 56%, rgba(242,242,239,1) 100%)';
   const bookStyle = {
     position: 'relative',
-    alignItems: 'flex-end',
-    display: 'flex',
-    width: '100%',
+    // alignItems: 'flex-end',
+    // display: 'flex',
     height: '100%',
-    // backgroundColor: '#f9d29d',
-    // zIndex: '100'
-// backgroundImage: 'linear-gradient(315deg, #f9d29d 0%, #ffd8cb 74%)'
+    width: '100%',
+    marginBottom: '93px',
+    // overflow: 'hidden'
+  
+    // minWidth: '90vw',
+    // minHeight: '100vh'
 
   };
-
+  
   let width = window.innerWidth > 900 ? window.innerWidth * .6 : window.innerWidth * .9;
   let height = window.innerHeight < 768 ? window.innerHeight * 1.2 : window.innerHeight * 1.11;
   let key = 0;
@@ -34,22 +37,27 @@ function PrettyCity() {
     <div className="pages">
       <Nav />
       <PagesContainer >
-
-        <FlipPage orientation='horizontal' uncutPages={true} height={JSON.stringify(height) - 102} width={JSON.stringify(width)} style={bookStyle}>
+    <Container fluid className = "page-container">
+        <FlipPage orientation='horizontal' uncutPages={true}  style={bookStyle} pageBackground={pageColor} perspective={'80em'} responsive={true} className="text-center">
           {pages.map(page => (
-            <article key={page.id} className="pretty-color">
+            <article key={page.id}>
               <div className='page-image' onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
-                <img src={page.imageLink} alt={page.imageTitle} className='imgs' key={key++} />
+                <Image src={page.imageLink} alt={page.imageTitle} className='imgs' key={key++} />
               </div>
-              <div className="text-center pretty-text pt-2">{page.text.map(line => (<h2 className="pretty-text" key={key++}>{line}</h2>))}</div>
+              {/* <div className="text-center pretty-text pt-2"> */}
+              {page.text.map(line => (<h2 className=" pretty-line" key={key++}>{line}</h2>))}
+              {/* </div> */}
             </article>
           ))}
         </FlipPage>
+        </Container>
       </PagesContainer>
     </div>
 
   );
 
 }
+
+// height={JSON.stringify(height) - 102} width={JSON.stringify(width)}
 
 export default PrettyCity
