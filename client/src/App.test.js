24f30748a0c70926f4d48740afbb7b0b26ce7a5b
kmop <42947972+AKluger@users.noreply.github.com>
2019-05-11 10:36:39 -0400

    initial test files

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import "./setupTest"
 import App from './App';
 
+
+describe("first enzyme test", () => {
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
 });
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+});
+
+// describe('Examining the syntax of Jest tests', () => {
+
+// it('sums numbers', () => {
+//   expect(1+2).toEqual(3);
+//   expect(2 +2).toEqual(4);
+// });
+
+// });
