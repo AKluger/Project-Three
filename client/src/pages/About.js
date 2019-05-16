ccb39bb39abcdad3a7b2d9cc94815a38873d7d4c
kmop <42947972+AKluger@users.noreply.github.com>
2019-05-15 22:12:00 -0400

    node-sass with initial scss and color variables

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from 'axios'
 import { Col, Container, Row, Jumbotron, Card, CardDeck } from 'react-bootstrap';
 import Nav from "../components/Nav";
-import './style.css';
+import './style.scss';
 import alex from './images/alex_youth.jpg';
 import monet from './images/monet_youth.jpg';
 import ron from './images/ronald_youth.jpg';
@@ -34,16 +34,16 @@ class About extends React.Component {
                     <Row className="justify-content-md-center p-1">
                         <Col md={8}>
                             <h1 className="h1-about"> Our Story: </h1>
-                            <h5>Problem: </h5><p>Our home city of Philadelphia has an epidemic of litter on the streets.  Faced with this dismal situation, we set out to have an impact that could raise awareness of the problem, engage the community to work towards a solution, and set our trajectory towards a cleaner future.  </p>
-                            <h5>Solution: </h5><p>This project began as a simple children's book idea that would send a positive message and encourage kids to avoid littering and pick up trash as they encounter it in the city.  With the power of digital storytelling and our abilities as web developers, the team expanded on this notion to produce a variety of books and games that could impact a diverse audience.  In addition, we have included resources for educators to provide feedback as we continue to build, and have built a framework for teachers to eventually track students' progress and learn more about opportunities to engage in cleanup efforts local to their schools.</p>
-                            <p>Thank you for taking the time to explore Winc-Stories, we hope it leaves you feeling optimistic about the future and more engaged with your community's efforts to prevent the trashing of our environment.</p></Col>
+                            <h4>Problem: </h4><p className="about-p">Our home city of Philadelphia has an epidemic of litter on the streets.  Faced with this dismal situation, we set out to have an impact that could raise awareness of the problem, engage the community to work towards a solution, and set our trajectory towards a cleaner future.  </p>
+                            <h4>Solution: </h4><p className="about-p">This project began as a simple children's book idea that would send a positive message and encourage kids to avoid littering and pick up trash as they encounter it in the city.  With the power of digital storytelling and our abilities as web developers, the team expanded on this notion to produce a variety of books and games that could impact a diverse audience.  In addition, we have included resources for educators to provide feedback as we continue to build, and have built a framework for teachers to eventually track students' progress and learn more about opportunities to engage in cleanup efforts local to their schools.</p>
+                            <p className="about-p">Thank you for taking the time to explore Winc-Stories, we hope it leaves you feeling optimistic about the future and more engaged with your community's efforts to prevent the trashing of our environment.</p></Col>
                     </Row>
                     <Row className="justify-content-md-center">
                         <CardDeck className="p-4">
                             <Card style={{ width: '18rem' }} className="creator">
                                 <Card.Img variant="top" className="creator-image" src={troy} alt="Troy Wynn" />
                                 <Card.Body>
-                                    <Card.Title>Troy Wynn</Card.Title>
+                                    <Card.Title id="creator-title">Troy Wynn</Card.Title>
                                     <Card.Subtitle className="mb-2" >Back-End Engineer</Card.Subtitle>
                                     <Card.Text></Card.Text>
                                     <a className="btn creator-btn" href="http://troy-wynn.com/" target="_blank" rel="noopener noreferrer">Portfolio</a >
@@ -52,7 +52,7 @@ class About extends React.Component {
                             <Card style={{ width: '18rem' }} className="creator">
                                 <Card.Img variant="top" src={monet} alt="Monet Thigpen" />
                                 <Card.Body>
-                                    <Card.Title>Monet Thigpen</Card.Title>
+                                    <Card.Title id="creator-title">Monet Thigpen</Card.Title>
                                     <Card.Subtitle className="mb-2" >Front-End Developer, UI Designer &amp; Author</Card.Subtitle>
                                     <Card.Text>
                                     </Card.Text>
@@ -62,7 +62,7 @@ class About extends React.Component {
                             <Card style={{ width: '18rem' }} className="creator">
                                 <Card.Img variant="top" className="creator-image" src={alex} alt="Alex Kluger" />
                                 <Card.Body>
-                                    <Card.Title>Alex Kluger</Card.Title>
+                                    <Card.Title id="creator-title">Alex Kluger</Card.Title>
                                     <Card.Subtitle className="mb-2" >Project Manager &amp; Author</Card.Subtitle>
                                     <Card.Text>
                                     </Card.Text>
@@ -72,7 +72,7 @@ class About extends React.Component {
                             <Card style={{ width: '18rem' }} className="creator">
                                 <Card.Img variant="top" className="creator-image" src={ron} alt="Ronald Glover" />
                                 <Card.Body>
-                                    <Card.Title>Ronald Glover</Card.Title>
+                                    <Card.Title id="creator-title">Ronald Glover</Card.Title>
                                     <Card.Subtitle className="mb-2" >React Developer</Card.Subtitle>
                                     <Card.Text>
                                     </Card.Text>
--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,20 +1,19 @@
 import React from "react";
 import { Col, Container, Row, Jumbotron, Badge } from 'react-bootstrap';
 import Nav from "../components/Nav";
-import './style.css'
+import './style.scss'
 
 class Welcome extends React.Component {
 
   render() {
     return (
       <div>
-        <Nav />
-
+        <Nav/>
         <Container fluid className="p-0">
           <Jumbotron id="hero-educator" />
           <Row className="justify-content-md-center">
             <Col md={8}>
-              <h1 className="welcome"><Badge variant="success">Welcome</Badge> we're Winc-Stories, a team of developers providing resources for parents and educators to encourage youth to keep their cities clean.  Kids can visit our <a href="/library">library</a> to discover a book or game.  Educators can sign up <a href="/signup"> here</a></h1>
+              <h1 className="welcome"><Badge id="welcome-badge">Welcome</Badge> we're Winc-Stories, a team of developers providing resources for parents and educators to encourage youth to keep their cities clean.  Kids can visit our <a href="/library" className="winc-link">library</a> to discover a book or game.  Educators please use our <a href="/signup" className="winc-link">sign-up page</a> for access to our feedback portal.</h1>
             </Col>
           </Row>
         </Container>
